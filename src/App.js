73f7440d23fbe2480bdea9b080e90c95cd998637
Ekaterina Erokhina <katerina.shimshek@gmail.com>
2023-11-16 23:37:29 +0300

    basic html & css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+        <form className="form">
+          <h1 className="form__heading">Sign In</h1>
+          <label for="email" className="form__label">Email</label>
+          <input id="email" type="email" placeholder="tony7989@gmail.com" className="form__input"></input>
+          <label for="password" className="form__label">Password</label>
+          <input id="password" placeholder="********" className="form__input"></input>
+          <button type="submit" className="form__button">Sign In</button>
+          <a href="/forgot-password" className="form__link">Forgot password?</a>
+        </form>
+      </div>
+    </main>
   );
 }
 
