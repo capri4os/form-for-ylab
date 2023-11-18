edf2b971056c8d5904ef37638ef94cd4c20e46d5
Ekaterina Erokhina <katerina.shimshek@gmail.com>
2023-11-18 15:36:44 +0300

    minor improvements

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
         <form className="form">
           <h1 className="form__heading">Sign In</h1>
           <label for="email" className="form__label">Email</label>
-          <input id="email" type="email" placeholder="tony7989@gmail.com" className="form__input"></input>
+          <input id="email" type="email" required placeholder="tony7989@gmail.com" className="form__input"></input>
           <label for="password" className="form__label">Password</label>
-          <input id="password" placeholder="********" className="form__input"></input>
+          <input id="password" type="password" required className="form__input"></input>
           <button type="submit" className="form__button">Sign In</button>
           <a href="/forgot-password" className="form__link">Forgot password?</a>
         </form>
